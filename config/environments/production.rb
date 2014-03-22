094f062a1448223ad8ba2e9c1bea05c5fbe82bed
WalkerCAdams <pamstal@yahoo.de>
2014-03-22 13:15:51 +0100

    -removed YandexBot from /config/environments/production ignore_crawlers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,14 +65,14 @@ Hek::Application.configure do
     :email_prefix => "[HEK Exception] ",
     :sender_address => %{ "HEK Exception" <exception@hek.uni-karlsruhe.de> },
     :exception_recipients => %w{ hekweb@hek.uni-karlsruhe.de },
-    :ignore_crawlers => %w{ Googlebot bingbot YandexBot}
+    :ignore_crawlers => %w{ Googlebot bingbot }
 
   config.middleware.use ExceptionNotification::Rack,
     :email => {
     :email_prefix => "[HEK ExceptionNotification] ",
     :sender_address => %{ "HEK Exception" <exception@hek.uni-karlsruhe.de> },
     :exception_recipients => %w{ hekweb@hek.uni-karlsruhe.de },
-    :ignore_crawlers => %w{ Googlebot bingbot YandexBot}
+    :ignore_crawlers => %w{ Googlebot bingbot }
   }
 
   Paperclip.options[:command_path] = '/usr/bin/'
